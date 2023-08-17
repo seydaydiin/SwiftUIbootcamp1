9a8ae51fde685135b3577d0ecfc22c8b3c5c53c5
Şeyda Aydın <seydaydiin0@gmail.com>
2023-08-17 22:40:32 +0300

    Initial Commit

---
+++ Bootcamp/BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootcampApp.swift
+//  Bootcamp
+//
+//  Created by Şeyda Aydın on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
